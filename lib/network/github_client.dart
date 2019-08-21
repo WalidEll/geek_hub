a8643e2b0bea768fe3b3418eda12bcb6c0db138f
Walid Elalaouy <walid.elalaouy@gmail.com>
2019-08-21 13:32:01 +0100

    adding page query

--- lib/network/github_client.dart
+++ lib/network/github_client.dart
@@ -13,8 +13,8 @@ class GithubClient {
     this.baseUrl = "https://api.github.com/search/repositories",
   }) : this.httpClient = httpClient ?? http.Client();
 
-  Future<SearchResult> search(String query,{String sort="",String order=""}) async {
-    final response = await httpClient.get(Uri.parse("$baseUrl?q=$query&sort=$sort&order=$order"));
+  Future<SearchResult> search(String query,{int page=1,String sort="",String order=""}) async {
+    final response = await httpClient.get(Uri.parse("$baseUrl?q=$query&sort=$sort&order=$order&page=$page"));
     final results = json.decode(response.body);
 
     if (response.statusCode == 200) {
